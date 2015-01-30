628a76dee4e670259c8c22956978ae20a128d098
Glenna Xie <x.jingge@gmail.com>
2015-01-29 22:48:05 -0700

    assignment 3 done

--- js/header.js
+++ js/header.js
@@ -1,8 +1,5 @@
 var header = document.querySelector(".flex-container");
 
-// var p = document.createElement("h1");
-// p.classList.add('flex-item');
-// header.appendChild(p);
 
 var h1 = document.createElement("h1");
 h1.classList.add('flex-item');
@@ -27,25 +24,14 @@ home.appendChild(homeLink);
 var resume = document.createElement('li');
 var resumeLink = document.createElement('a');
 resumeLink.textContent = 'resume';
-var leftBracket = document.createElement('span');
-leftBracket.classList.add('hoverEffect');
-leftBracket.textContent = '< ';
-
-var rightBracket = document.createElement('span');
-rightBracket.classList.add('hoverEffect');
-rightBracket.textContent = ' >';
 resumeLink.href = "resume.html";
 menu.appendChild(resume);
-resume.appendChild(leftBracket);
 resume.appendChild(resumeLink);
-resume.appendChild(rightBracket);
 
 var blog = document.createElement('li');
 var blogLink = document.createElement('a');
 blogLink.textContent = 'blog';
 blogLink.href = "blog.html";
 menu.appendChild(blog);
-blog.appendChild(leftBracket);
 blog.appendChild(blogLink);
-blog.appendChild(rightBracket);
 
